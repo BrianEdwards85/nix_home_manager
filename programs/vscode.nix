b300d6bbedb1adca475948c29daa42d74abf51a4
Brian Edwards <brian@edwardstx.us>
2026-01-25 21:39:26 -0600

    VSCode

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -8,14 +8,35 @@
       "workbench.colorTheme" = "Cobalt2";
     };
     profiles.default.extensions = with pkgs.vscode-extensions; [
-#      wesbos.theme-cobalt2
 #      lukevanlukevan.qmk-action-button
       esbenp.prettier-vscode
       pkief.material-icon-theme
-#      esphone.esphome-vscode
-#      dracula-theme.theme-dracula
       vscodevim.vim
-#      yzhang.markdown-all-in-one
+    ]
+# nix store prefetch-file https://marketplace.visualstudio.com/_apis/public/gallery/publishers/lukevan/vsextensions/qmk-action-button/0.1.5/vspackage
+#    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+#      {
+#        name = "";
+#        publisher = "";
+#        version = "";
+#        sha256 = "";
+#      }
+#    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "qmk-action-button";
+        publisher = "lukevan";
+        version = "0.1.5";
+        sha256 = "bz7is2sEJJBQHGrxEoR2SY1pZ/VV6X18OrdooxGz57o=";
+      }
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "nix-ide";
+        publisher = "jnoortheen";
+        version = "0.5.5";
+        sha256 = "epdEMPAkSo0IXsd+ozicI8bjPPquDKIzB3ONRUYWwn8=";
+      }
     ]
     ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
       {
@@ -23,6 +44,14 @@
         publisher = "wesbos";
         version = "2.5.0";
         sha256 = "niIsC1J1pX93GwM6Fff/spk/p8qvBVDRxR7EO/tfcHc=";
+      }
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "esphome-vscode";
+        publisher = "esphome";
+        version = "2025.7.0";
+        sha256 = "HxM/spKzLvyfuDi4BIBCU+z9cDD4h0WZ4qtAqIhkILs=";
       }
     ];
   };
