c530fb2a18a7ee41fae584ce04805cc7f49e8e7b
Brian Edwards <brian@edwardstx.us>
2026-01-25 20:49:21 -0600

    Extentions

--- programs/vscode.nix
+++ programs/vscode.nix
@@ -3,7 +3,12 @@
 {
   programs.vscode = {
     enable = true;
+    profiles.default.userSettings = {
+      "workbench.iconTheme" = "material-icon-theme";
+      "workbench.colorTheme" = "Cobalt2";
+    };
     profiles.default.extensions = with pkgs.vscode-extensions; [
+#      wesbos.theme-cobalt2
 #      lukevanlukevan.qmk-action-button
       esbenp.prettier-vscode
       pkief.material-icon-theme
@@ -11,6 +16,14 @@
 #      dracula-theme.theme-dracula
       vscodevim.vim
 #      yzhang.markdown-all-in-one
+    ]
+    ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "theme-cobalt2";
+        publisher = "wesbos";
+        version = "2.5.0";
+        sha256 = "niIsC1J1pX93GwM6Fff/spk/p8qvBVDRxR7EO/tfcHc=";
+      }
     ];
   };
 }
