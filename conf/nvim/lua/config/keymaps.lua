1d5a6b9e1970c0cc0a9fc060741d59e3cd02176f
Brian Edwards <bedwards@indeed.com>
2026-02-09 10:09:08 -0600

    Nvim Keymaps

--- conf/nvim/lua/config/keymaps.lua
+++ conf/nvim/lua/config/keymaps.lua
@@ -2,7 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- vim.keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to left window", remap = true })
--- vim.keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
--- vim.keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
--- vim.keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("t", "<C-Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<leader><Left>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<leader><Right>", "<C-w>l", { desc = "Go to right window", remap = true })
+vim.keymap.set("n", "<leader><Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<leader><Down>", "<C-w>j", { desc = "Go to lower window", remap = true })
