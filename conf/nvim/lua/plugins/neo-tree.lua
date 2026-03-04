b5320ab0f437266be7be0706f5dfa5ebb97f693e
Brian Edwards <bedwards@indeed.com>
2026-03-04 10:54:57 -0600

    Git keyboard shortcuts

---
+++ conf/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      commands = {
+        git_add = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.system({ "git", "add", path })
+          require("neo-tree.sources.manager").refresh("filesystem")
+        end,
+        git_unstage = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          vim.fn.system({ "git", "restore", "--staged", path })
+          require("neo-tree.sources.manager").refresh("filesystem")
+        end,
+      },
+      window = {
+        mappings = {
+          ["<leader>ga"] = "git_add",
+          ["<leader>gu"] = "git_unstage",
+        },
+      },
+    },
+  },
+}
